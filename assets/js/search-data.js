f494942e7442e5205e78c29c24f5ee1c310d6bd2
yfyuan01 <yfyuan01@gmail.com>
2025-05-26 21:31:13 +0000

    Deploying to gh-pages from @ yfyuan01/yfyuan01.github.io@069464fdd6eaccb1f4746d71ba41f1048d611fd4 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -30,8 +30,8 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/teaching/";
           },
-        },{id: "nav-",
-          title: "",
+        },{id: "nav-service",
+          title: "service",
           description: "",
           section: "Navigation",
           handler: () => {
