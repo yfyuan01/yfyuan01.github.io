e4b78ba8538563ed523f8cd7185731d4233ff7e6
yfyuan01 <yfyuan01@gmail.com>
2025-05-26 21:04:17 +0000

    Deploying to gh-pages from @ yfyuan01/yfyuan01.github.io@8d8e6e204c0166b16a964647c0f2d0275e91747b 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -30,6 +30,13 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/teaching/";
           },
+        },{id: "nav-service",
+          title: "service",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/service/";
+          },
         },{id: "post-a-post-with-plotly-js",
         
           title: "a post with plotly.js",
